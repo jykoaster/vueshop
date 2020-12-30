63b87b945de627e78b1d3a431d7ac0ce6421e7aa
jyx <jypc@xxx.cc>
2020-12-30 17:04:10 +0800

    update login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,9 +2,11 @@
   <v-main>
     <v-container>
       <v-form ref="form">
-        <v-text-field label="Account" required></v-text-field>
-        <v-text-field type="password" label="Password" required></v-text-field>
-        <v-btn color="success" class="mr-4" @click="login">
+        <v-text-field v-model="acc" label="Account" required></v-text-field>
+        <v-text-field v-model="pwd" type="password" label="Password" required></v-text-field>
+        <v-text-field v-model="cap" label="Captcha" required></v-text-field>
+        <v-img :src="img" @click="captcha()"></v-img>
+        <v-btn color="success" class="mr-4" @click="login()">
           Login
         </v-btn>
       </v-form>
@@ -12,14 +14,33 @@
   </v-main>
 </template>
 <script>
+import Vue from 'vue'
 export default {
   data: () => ({
-
+    acc: '',
+    pwd: '',
+    cap: '',
+    key: '',
+    img: null,
   }),
+  mounted: function() {
+    this.captcha()
+  },
   methods: {
     login() {
+      Vue.axios.post('/api/v1/login', { account: this.acc, password: this.pwd, captcha: this.cap, key: this.key }).then(() => {
+        alert('success')
+      }).catch((error) => {
+        alert(error.response.data.error)
+      })
+    },
+    captcha() {
+      Vue.axios.get('/api/v1/captcha/math').then((response) => {
+        this.img = response.data.img
+        this.key = response.data.key
+      })
+    },
 
-    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
-   publicPath: process.env.NODE_ENV === 'production'
+  ],
+  devServer: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://10.1.0.12',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/vueshop/'
     : '/'
 }
